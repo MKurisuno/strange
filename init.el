e18052419db0e77e42c457e5c79b7f61666e0b30
Makoto Kurisuno <makoto.kurisuno@gmail.com>
2026-08-01 23:48:17 +0900

    init.el を更新

--- init.el
+++ init.el
@@ -369,7 +369,7 @@
 	  '(treemacs-collapse-dirs                   (if treemacs-python-executable 3 0) )
 	  '(treemacs-deferred-git-apply-delay        0.5)
 	  '(treemacs-directory-name-transformer      #'identity)
-	  '(treemacs-display-in-side-window          t	)
+	  '(treemacs-display-in-side-window          t)
           '(treemacs-eldoc-display                   'simple)
           '(treemacs-file-event-delay                2000)
           '(treemacs-file-extension-regex            treemacs-last-period-regex-value)
