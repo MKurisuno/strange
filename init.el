f17d5fa6f49bffa5814ba5018ba28e11e2196bec
M.Kurisuno <makoto.kurisuno@gmail.com>
2026-09-11 18:36:44 +0900

    init.el Update

--- init.el
+++ init.el
@@ -267,12 +267,11 @@
 	       (corfu-popupinfo-delay . nil)
 	       (corfu-cycle . t)
 	       (corfu-quit-no-match 'separator)
-           ;; 補完ソースの順序を指定
-           (corfu-sources . '(
-			   corfu-lsp
-			   corfu-dabbrev
-			   corfu-dict
-			   corfu-yasnippet)))
+	       ;; 補完ソースの順序を指定
+	       (corfu-sources . '(corfu-lsp
+				  corfu-dabbrev
+				  corfu-dict
+				  corfu-yasnippet)))
   :bind   ((corfu-map
           ("C-s" . corfu-insert-separator))))
 
